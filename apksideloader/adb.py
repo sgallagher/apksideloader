f1355664b32c966121ba2592ff33e204e785e00f
Stephen Gallagher <sgallagh@redhat.com>
2020-06-03 07:49:01 -0400

    Fix adb interactions
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- apksideloader/adb.py
+++ apksideloader/adb.py
@@ -4,13 +4,11 @@ import random
 import string
 
 from adb_shell.adb_device import AdbDeviceTcp
-from adb_shell.auth.sign_pythonrsa import PythonRSASigner
-from adb_shell.exceptions import DeviceAuthError
-
+from adb_shell.exceptions import DeviceAuthError, TcpTimeoutException
 
 logger = logging.getLogger(__name__)
 
-CROSTINI_DEFAULT_ADB_SERVER='100.115.92.2'
+CROSTINI_DEFAULT_ADB_SERVER = '100.115.92.2'
 CROSTINI_DEFAULT_ADB_PORT = 5555
 
 
@@ -33,19 +31,19 @@ class AdbConnection:
         self.signer = signer
 
     def connect(self):
-        adb_device = AdbDeviceTcp(self.server, self.port)
+        self.device = AdbDeviceTcp(self.server, self.port)
 
         try:
             if self.signer:
-                adb_device.connect(rsa_keys=[self.signer], timeout_s=10.0)
+                self.device.connect(rsa_keys=[self.signer], timeout_s=10.0)
             else:
-                adb_device.connect(timeout_s=10.0)
+                self.device.connect(timeout_s=10.0)
 
         except OSError as e:
             logger.error("OSError: {}".format(e))
             raise AdbConnectionError(e)
 
-        except DeviceAuthError as e:
+        except (DeviceAuthError, TcpTimeoutException) as e:
             logger.error("Could not authenticate to the ADB server: {}".format(e))
             raise AdbConnectionError(e)
 
@@ -58,14 +56,14 @@ class AdbConnection:
         logger.info("dst_filename: {}".format(dst_filename))
 
         dst_path = os.path.join(AdbConnection.TMP_PATH, dst_filename)
-        self.push(apk.src_path, dst_path, total_timeout_s=60.0, progress_callback=apk.update_progress)
+        self.device.push(apk.src_path, dst_path, total_timeout_s=10.0, progress_callback=apk.update_progress)
         apk.progress.set_fraction(1.0)
 
         apk.info('Installing...')
 
         # Install the APK on the target
         try:
-            output = self.shell('pm install -r {}'.format(dst_path))
+            output = self.device.shell('pm install -r {}'.format(dst_path))
         except Exception as e:
             logger.error("Installation failed: {}".format(e))
             raise
