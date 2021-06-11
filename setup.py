69c5ecf4e9dd8fe2f282b23939de982455f4fad6
Stephen Gallagher <sgallagh@redhat.com>
2021-06-10 22:33:00 -0400

    Lighten crypto dependency
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         "PyGObject~=3.40.1",
         "click~=8.0.1",
         "apksideloader~=0.2",
-        "cryptography~=3.4.7",
+        "cryptography",
     ],
     entry_points={
         "console_scripts": [
