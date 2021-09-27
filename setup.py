f9b6e5b1fe9991e51973becd5cd088acc525fd4a
Stephen Gallagher <sgallagh@redhat.com>
2021-09-27 09:23:22 -0400

    Use modern setuptools config
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

--- setup.py
+++ setup.py
@@ -1,53 +1,6 @@
-from setuptools import setup
+#!/usr/bin/env python
 
-setup(
-    name="apksideloader",
-    version="0.2",
-    license="GPLv3+",
-    description="A tool to sideload Android packages onto Chromebooks",
-    keywords="android apk sideload",
-    # From https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        # Development status
-        "Development Status :: 3 - Alpha",
-        # Target audience
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        # Type of software
-        "Topic :: System :: Installation/Setup",
-        "Topic :: System :: Software Distribution",
-        # Kind of software
-        "Environment :: Console",
-        "Environment :: X11 Applications :: GTK",
-        # License (must match license field)
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        # Operating systems supported
-        "Operating System :: POSIX",
-        "Operating System :: POSIX :: Linux",
-        # Supported Python versions
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-    ],
-    url="http://github.com/sgallagher/apksideloader",
-    author="Stephen Gallagher",
-    author_email="Stephen@gallagherhome.com",
-    packages=["apksideloader"],
-    include_package_data=True,
-    install_requires=[
-        "setuptools~=49.6.0",
-        "PyGObject~=3.40.1",
-        "click~=8.0.1",
-        "apksideloader~=0.2",
-        "cryptography",
-    ],
-    entry_points={
-        "console_scripts": [
-            "apksideloader = apksideloader.cli:main",
-        ],
-    },
-    data_files=[
-        ('share/applications', ['data/com.gallagherhome.apksideloader.desktop']),
-        ('share/icons', ['data/icons/hicolor/192x192/android_icon.png']),
-    ],
-)
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
